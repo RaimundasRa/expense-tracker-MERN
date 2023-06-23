6a5d599be470a2cdfd42e1a68c5ba2d7b233cbeb
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-06-23 11:51:07 +0100

    building app components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </>
+  );
 }
 
 export default App;
